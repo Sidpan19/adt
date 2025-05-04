573bdaad612ce90bbe4b13dd487bc2167dad1241
Gowtham Reddy <gowtham.rdy2001@gmail.com>
2025-05-04 17:54:59 -0400

    added remote connection

--- connection.py
+++ connection.py
@@ -1,18 +1,30 @@
+import os
 from flask import Flask, render_template, g
 from py2neo import Graph
-import os
 from dotenv import load_dotenv
-load_dotenv
-def get_db():
-    if not hasattr(g, 'Graph DBMS'):
-        g.neo4j_db = Graph(
-            os.getenv('ADT'),
-            auth=(os.getenv('neo4j'), os.getenv('password')),
-            name=os.getenv('DATABASE')  
 
-        )
+# load .env into os.environ
+load_dotenv()
+
+def get_db():
+    """
+    Lazily create (and cache) a Graph instance on g.neo4j_db.
+    """
+    if 'neo4j_db' not in g:
+        uri      = os.getenv("NEO4J_URI")
+        user     = os.getenv("NEO4J_USERNAME")
+        password = os.getenv("NEO4J_PASSWORD")
+        database = os.getenv("NEO4J_DATABASE", "neo4j")
+        g.neo4j_db = Graph(uri, auth=(user, password), name=database)
     return g.neo4j_db
 
-def close_db(error):
-    if hasattr(g, 'Graph DBMS'):
-        g.neo4j_db = None
+def close_db(error=None):
+    """
+    Teardown handler to close and remove the Graph instance.
+    """
+    graph = g.pop('neo4j_db', None)
+    if graph is not None:
+        try:
+            graph.close()
+        except Exception:
+            pass
